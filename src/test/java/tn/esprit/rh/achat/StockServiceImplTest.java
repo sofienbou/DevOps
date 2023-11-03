c32ac690173e25bbcbfe5af68ad44b1ffee2e063
yepdoleslie <lesliebrielle.yepdomonthe@esprit.tn>
2023-11-04 00:04:25 +0100

    modif src

--- src/test/java/tn/esprit/rh/achat/StockServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/StockServiceImplTest.java
@@ -1,19 +1,19 @@
-package tn.esprit.rh.achat;
+package tn.esprit.rh.achat.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.rh.achat.entities.Stock;
 import tn.esprit.rh.achat.repositories.StockRepository;
-import tn.esprit.rh.achat.services.StockServiceImpl;
-
-import java.util.Arrays;
-import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Optional;
 
+@ExtendWith(MockitoExtension.class)
 
 public class StockServiceImplTest {
 
@@ -22,74 +22,64 @@ public class StockServiceImplTest {
 
     @Mock
     private StockRepository stockRepository;
-@Test 
-    public void testRetrieveAllStocks(){
-    List<Stock> fakeStocks =  Arrays.asList(
-            new Stock(1L, "Stock 1", 100, 10, null),
-            new Stock(2L, "Stock 2", 200, 20, null)
-    );
 
-    Mockito.when(stockRepository.findAll()).thenReturn(fakeStocks);
-    List<Stock> result = stockService.retrieveAllStocks();
-    Assertions.assertEquals(fakeStocks.size(), result.size());
 
-    }
+
     @Test
     public void testAddStock() {
+        // Créez un faux Stock pour simuler la réponse du repository
         Stock fakeStock = new Stock(3L, "Stock 3", 150, 15, null);
 
+        // Configurez le comportement du repository mock
         Mockito.when(stockRepository.save(Mockito.any(Stock.class))).thenReturn(fakeStock);
 
+        // Appelez la méthode de service que vous souhaitez tester
         Stock result = stockService.addStock(fakeStock);
 
-        Assertions.assertEquals(fakeStock, result);
+        // Vérifiez si le résultat est conforme à vos attentes
+        assertEquals(fakeStock, result);
     }
-
     @Test
     public void testDeleteStock() {
+        // Identifiant du stock à supprimer
         Long stockId = 1L;
 
+        // Appelez la méthode de service pour supprimer le stock
         stockService.deleteStock(stockId);
 
+        // Vérifiez si la méthode du repository mock a été appelée une fois pour supprimer le stock avec l'identifiant donné
         Mockito.verify(stockRepository, Mockito.times(1)).deleteById(stockId);
     }
 
     @Test
     public void testUpdateStock() {
-        Stock fakeStock = new Stock(1L, "Updated Stock 9", 120, 12, null);
+        // Créez un faux stock avec les nouvelles valeurs
+        Stock fakeStock = new Stock(1L, "Updated Stock 1", 120, 12, null);
 
+        // Configurez le comportement du repository mock pour sauvegarder le faux stock
         Mockito.when(stockRepository.save(Mockito.any(Stock.class))).thenReturn(fakeStock);
 
+        // Appelez la méthode de service pour mettre à jour le stock
         Stock result = stockService.updateStock(fakeStock);
 
+        // Vérifiez si le stock retourné par la méthode de service est égal au faux stock
         Assertions.assertEquals(fakeStock, result);
     }
-
     @Test
     public void testRetrieveStock() {
-        Long stockId = 1L;
-        Stock fakeStock = new Stock(stockId, "Stock 8", 100, 10, null);
+        // Créez un faux Stock pour simuler la réponse du repository
+        Stock fakeStock = new Stock(1L, "Stock de test", 100, 10, null);
 
-        Mockito.when(stockRepository.findById(stockId)).thenReturn(Optional.of(fakeStock));
+        // Configurez le comportement du repository mock
+        Mockito.when(stockRepository.findById(1L)).thenReturn(Optional.of(fakeStock));
 
-        Stock result = stockService.retrieveStock(stockId);
+        // Appelez la méthode de service que vous souhaitez tester
+        Stock result = stockService.retrieveStock(1L);
 
-        Assertions.assertEquals(fakeStock, result);
+        // Vérifiez si le résultat est conforme à vos attentes
+        assertEquals(fakeStock, result);
     }
 
-    @Test
-    public void testRetrieveStatusStock() {
-        // Mettez en place votre test avec les données appropriées
-        // Assurez-vous de couvrir différents cas de votre méthode
 
-        // Exemple :
-        Stock fakeStock = new Stock(1L, "Stock 7", 5, 10, null);
-
-        Mockito.when(stockRepository.retrieveStatusStock()).thenReturn(Arrays.asList(fakeStock));
-
-        String result = stockService.retrieveStatusStock();
-
-        Assertions.assertNotNull(result);
-    }
-
+    // Ajoutez d'autres méthodes de test au besoin
 }
